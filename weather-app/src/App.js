6a7431b9c9360dec4b2941679a312c03c34217df
zor009 <anzohra@outlook.com>
2019-11-16 16:57:01 -0500

    react

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  render(){
   return (
     <div className="App">
      
     </div>
-  );
+  )};
 }
 
 export default App;
