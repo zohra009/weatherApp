fe142daa8b7cf22a7a8116f2bd81edd2c4afae06
zor009 <anzohra@outlook.com>
2019-11-16 17:05:32 -0500

    title

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render(){
   return (
     <div className="App">
-     
+     <h1>hello sexy weather app</h1>
     </div>
   )};
 }
---
+++ weather-app/src/components/Titles.js
@@ -0,0 +1,2 @@
+import React, { Component } form 'react'; 
+
