c63a98b38404babca0aae3a26700d675df82e85e
zor009 <anzohra@outlook.com>
2019-12-07 15:15:34 -0500

    weather component

---
+++ weather-app/src/components/Weather.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Weather extends Component {
+
+    return(){
+        return (
+            <div>
+                In Weather Page!
+            </div>
+        )
+    }
+}
+export default Weather;
