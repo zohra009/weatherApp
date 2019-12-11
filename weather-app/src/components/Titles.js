2bdbd63d505e87db1e5227b3f96e798ef35cdf4a
zor009 <anzohra@outlook.com>
2019-12-11 00:26:47 -0500

    new info in title

--- weather-app/src/components/Titles.js
+++ weather-app/src/components/Titles.js
@@ -4,7 +4,7 @@ class Titles extends Component {
 render() {
 return (
     <div>
-        <h1> Weather Finder</h1>
+        <h1> Weather Finder title page </h1>
         <p> Find out the temperature, conditions and more!</p>
     </div>
 }
